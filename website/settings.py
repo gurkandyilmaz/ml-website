5c1c60f0993546c6e26e6ad4a0e8d42e19409ee7
gurkan yilmaz <gurkandy@gmail.com>
2020-05-03 19:30:26 +0300

    0.0.0.0 added to allowed_hosts

--- website/settings.py
+++ website/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '+wlzc(%+sutn4kzez7h3mb455-jn)b-bxwjt^j0wxx86ieu&en'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com' , '0.0.0.0']
 
 
 # Application definition
