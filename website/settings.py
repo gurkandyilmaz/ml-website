4adb7fdb43541d71d704529dc33a296951ddfa0e
gurkan yilmaz <gurkandy@gmail.com>
2020-05-09 23:12:45 +0300

    seetings py

--- website/settings.py
+++ website/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '+wlzc(%+sutn4kzez7h3mb455-jn)b-bxwjt^j0wxx86ieu&en'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # DEBUG = True
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com' , '0.0.0.0', 'localhost']
 
