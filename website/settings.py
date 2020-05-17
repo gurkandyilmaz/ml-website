8ecf6730bfba235af6f9a6df9b259473067de5af
gurkan yilmaz <gurkandy@gmail.com>
2020-05-18 00:51:10 +0300

    model 1, css styling

--- website/settings.py
+++ website/settings.py
@@ -129,11 +129,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_URL = '/static/'
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (STATIC_DIR, )
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
